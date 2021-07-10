8d8e86a4aab95a49ad6c9366a8f31a174dd821b2
SergiuR <serjik1994@gmail.com>
2021-07-11 00:03:29 +0300

    Добавлено модальное окно. Переработан интерфейс.

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const button = document.querySelector('#newNoteButton');
+const modal = document.querySelector('#modal');
+const text = document.querySelector('#textNewNote');
 let noteNum = 0;
 
 
 button.onclick = () => {
-  newNote = prompt('Новая заметка');
-  if (newNote === '' || newNote === null) {
+  modal.setAttribute('class', 'modalDiv');
+}
+
+function saveNote() {
+  if (text.value === '') {
     return;
   }
   const container = document.createElement('div');
@@ -12,16 +17,22 @@ button.onclick = () => {
   container.setAttribute('class', 'noteDiv');
   const note = document.createElement('p');
   note.setAttribute('class', 'textNote');
-  note.innerHTML = newNote;
+  note.innerHTML = text.value;
   deleteButton = document.createElement('button');
   deleteButton.innerHTML = '&times';
   deleteButton.setAttribute('class', 'delButton');
-  deleteButton.setAttribute('onclick', `delNote(${noteNum})`);
+  deleteButton.setAttribute('onclick', `delNote(${text.value})`);
   noteNum += 1;
   document.body.append(container);
   container.append(note);
   container.append(deleteButton);
+  modal.setAttribute('class', 'displayOff');
+  text.value = '';
+}
 
+function cancel() {
+  modal.setAttribute('class', 'displayOff');
+  text.value = '';
 }
 
 function delNote(idNote) {
