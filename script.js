2477c1e0d488b0e2089f1e856917ace74c9acc36
SergiuR <serjik1994@gmail.com>
2021-07-11 00:26:41 +0300

    реализовано локальное хранение заметок

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 const button = document.querySelector('#newNoteButton');
 const modal = document.querySelector('#modal');
 const text = document.querySelector('#textNewNote');
+const noteStorage = window.localStorage;
 let noteNum = 0;
+
+function loadNotes() {
+  for (var i = 1; i <= noteStorage.length; i++) {
+    let textNote = noteStorage[i];
+    const container = document.createElement('div');
+    container.setAttribute('id', i);
+    container.setAttribute('class', 'noteDiv');
+    const note = document.createElement('p');
+    note.setAttribute('class', 'textNote');
+    note.innerHTML = textNote;
+    deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '&times';
+    deleteButton.setAttribute('class', 'delButton');
+    deleteButton.setAttribute('onclick', `delNote(${i})`);
+    document.body.append(container);
+    container.append(note);
+    container.append(deleteButton);
+    noteNum = i;
+  }
+  noteNum++;
+}
 
 
 button.onclick = () => {
@@ -21,11 +43,13 @@ function saveNote() {
   deleteButton = document.createElement('button');
   deleteButton.innerHTML = '&times';
   deleteButton.setAttribute('class', 'delButton');
-  deleteButton.setAttribute('onclick', `delNote(${text.value})`);
-  noteNum += 1;
+  deleteButton.setAttribute('onclick', `delNote(${noteNum})`);
+  noteStorage.setItem(noteNum, text.value);
+
   document.body.append(container);
   container.append(note);
   container.append(deleteButton);
+  noteNum += 1;
   modal.setAttribute('class', 'displayOff');
   text.value = '';
 }
@@ -37,5 +61,6 @@ function cancel() {
 
 function delNote(idNote) {
   const delDiv = document.getElementById(idNote);
+  noteStorage.removeItem(idNote);
   delDiv.remove();
 }
